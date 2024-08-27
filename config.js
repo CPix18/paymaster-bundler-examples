93fa61d71a0f2558db187274050b66e0b16eef87
Collin Pixley <39272924+CPix18@users.noreply.github.com>
2024-08-27 12:14:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export default {
     "rpc_url": process.env.RPC_URL,
     "private_key": process.env.PRIVATE_KEY,
     "account_type": process.env.ACCOUNT_TYPE || 'simple',
-    "contract_address": "0x66519FCAee1Ed65bc9e0aCc25cCD900668D3eD49",
-    "function_name": "mintTo",
+    "contract_address": "0xdf3Cf7F9923a6D19D3765206Fa39a4174E5AD1F4",
+    "function_name": "requestTokens",
     "entry_point": "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
 }
